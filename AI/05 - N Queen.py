17546a2b73a8f5acbaf4fcc43c784ca6e188635a
Naitik Fulfagar <naitiknfulfagar@gmail.com>
2024-05-05 12:10:04 +0530

    Updated

--- AI/05 - N Queen.py
+++ AI/05 - N Queen.py
@@ -1,18 +1,20 @@
 def isSafe(arr,x,y,n) ->bool:
+    
+    # Check if there's a queen in the same column above
     for row in range(x):
         if arr[row][y]==1:
             return False
-        
-    row, col = x, y
     
+    # Check upper left diagonal    
+    row, col = x, y
     while row >= 0 and col >= 0:
         if arr[row][col]==1:
             return False
         row -= 1
         col -= 1
         
+    # Check upper right diagonal
     row, col = x, y
-    
     while row >= 0 and col < n:
         if arr[row][col]==1:
             return False
@@ -38,13 +40,15 @@ def nqueen(arr,x,n) -> bool:
     return False
 
 if __name__=="__main__":
-    n = int(input("Enter N for N-Queen Problem "))
+    n   = int(input("Enter N for N-Queen Problem : "))
     arr = [[0 for _ in range(n)] for _ in range(n)]
-    print("\n\nHere is Solution for",n,"Queens\n\n")
-    if (nqueen(arr,0,n)):
-        for i in range(n):
+    
+    if nqueen(arr,0,n)  :
+        print("\n\nHere is Solution for",n,"Queens\n\n")
+        for i in range(n)   :
             for j in range(n):
                 print(arr[i][j] ,end="\t")
-                
-            print("\n") 
-    
+            print("\n")
+            
+    else:
+        print("\n\nSolution does not exist for the given ",n,"\n\n")
---
+++ AI/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+for i in range(n)   :
+            for j in range(n):
+                print(arr[i][j] ,end="\t")
+                
+            print("\n")
